df8ebe18fc70bc766addca63a1137895c86dfe5b
Kyle Martin <kyle.martin.0819@gmail.com>
2023-03-30 14:29:46 -0400

    Type  Conversions Assignment

---
+++ CSharp-Mastercourse/Applications/Console Applications/App 2/Readme.md
@@ -0,0 +1,11 @@
+# Type Conversions
+
+## Assignment
+
+- Capture the Users Age
+- Display in natural language how old they will be in 25 years
+- Display in natural language how old they were 25 years ago
+
+## App Example
+
+!link
---
+++ CSharp-Mastercourse/Applications/Console Applications/App 2/TypeConversions/Program.cs
@@ -0,0 +1,12 @@
+﻿int age = 0;
+int years = 25;
+
+Console.Write("What is your age: ");
+string? ageText = Console.ReadLine();
+bool isValidInput = int.TryParse(ageText, out age);
+
+Console.WriteLine(isValidInput ? $"Your age is {age}." : "Your input is not valid.");
+
+int futureAge = age + years;
+int pastAge = age - years;
+Console.WriteLine($"Twenty five years from now you will be {futureAge} years old. Twenty five years ago you were {pastAge} years old.");
