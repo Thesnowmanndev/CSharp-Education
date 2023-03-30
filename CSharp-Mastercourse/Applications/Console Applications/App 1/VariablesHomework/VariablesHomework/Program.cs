d461c20451b89457780ce6f8c87c3741456aafa4
Kyle Martin <kyle.martin.0819@gmail.com>
2023-03-30 11:36:09 -0400

    Homework Assignment

---
+++ CSharp-Mastercourse/Applications/Console Applications/App 1/VariablesHomework/VariablesHomework/Program.cs
@@ -0,0 +1,10 @@
+﻿string firstName = "Kyle";
+string lastName = "Martin";
+int age = 33;
+bool alive = true;
+string phoneNumber = "12345678900";
+
+Console.WriteLine($"Users name: {firstName} {lastName}.");
+Console.WriteLine($"Users age: {age}.");
+Console.WriteLine(alive ? "User is alive." : "User is dead.");
+Console.WriteLine($"{phoneNumber[0]}-{phoneNumber[1]}{phoneNumber[2]}{phoneNumber[3]}-{phoneNumber[4]}{phoneNumber[5]}{phoneNumber[6]}-{phoneNumber[7]}{phoneNumber[8]}{phoneNumber[9]}{phoneNumber[10]}");
---
+++ CSharp-Mastercourse/Applications/Console Applications/App 1/readme.md
@@ -0,0 +1,12 @@
+# Variables Homework
+
+## Application Requirements
+
+- Console application
+- Store the users name
+- Store the users age
+- Store whether or not the user is alive or dead
+- Store the users phone number
+
+### Preview
+![Application Preview](app-output.png)
