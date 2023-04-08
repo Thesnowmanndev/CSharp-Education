7516e41356d0fe29294ee2b203f9f3bbff881976
Kyle Martin <kyle.martin.0819@gmail.com>
2023-04-08 14:48:52 -0400

    Update Program.cs

--- CSharp-Mastercourse/Applications/Console Applications/App 5 - Do While Homework/DoWhileHomework/Program.cs
+++ CSharp-Mastercourse/Applications/Console Applications/App 5 - Do While Homework/DoWhileHomework/Program.cs
@@ -6,13 +6,22 @@ do
     Console.Write("What is your name: ");
     name = Console.ReadLine().ToLower();
 
-    if (name == "kyle") { Console.WriteLine($"Welcome Admin {name}."); }
-    else { Console.WriteLine($"Welcome User {name}."); }
+    if (name == "kyle") 
+    { 
+        Console.WriteLine($"Welcome Admin {name}."); 
+    }
+    else 
+    { 
+        Console.WriteLine($"Welcome User {name}."); 
+    }
 
     string exitText = String.Empty;
     Console.Write("To exit the application, type exit: ");
     exitText = Console.ReadLine().ToLower();
 
-    if(exitText == "exit") { exit = true; }
+    if (exitText == "exit") 
+    { 
+        exit = true; 
+    }
 
 }  while (!exit);
