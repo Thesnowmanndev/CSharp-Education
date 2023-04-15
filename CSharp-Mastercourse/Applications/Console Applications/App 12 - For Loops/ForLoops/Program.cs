49a54b3e5f9d7a8962a6a463830a0e06032b0b97
Kyle Martin <kyle.martin.0819@gmail.com>
2023-04-15 14:46:58 -0400

    For Loops

---
+++ CSharp-Mastercourse/Applications/Console Applications/App 12 - For Loops/ForLoops/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-for-statement for more
+
+// Dummy Data
+List<string> data = new List<string>();
+data.Add("Kyle");
+data.Add("Tom");
+data.Add("Dan");
+data.Add("Albert");
+data.Add("Fred");
+
+// Simple for loop
+for (int i = 0; i < data.Count; i++)
+{
+    Console.WriteLine(data[i]);
+}
+
+// Dummy Data 2
+List<decimal> data2 = new();
+data2.Add(22.71M);
+data2.Add(1.59M);
+data2.Add(67.53M);
+data2.Add(42.87M);
+data2.Add(3.37M);
+
+decimal total = 0;
+
+// Math with for loops
+for (int i = 0; i < data2.Count;i++)
+{
+    total += data2[i];
+}
+
+Console.WriteLine($"Total Cost: ${total}");
