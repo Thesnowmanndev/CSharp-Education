6fcc783445e6dd2c6a784089ad5e978ee96eaba8
Kyle Martin <kyle.martin.0819@gmail.com>
2023-04-15 17:16:48 -0400

    Foreach Homework

---
+++ CSharp-Mastercourse/Applications/Console Applications/App 15 - For Each Loop Homework/ForEachLoopHomework/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Employee Roster Creator");
+
+List<string> firstNames = new();
+bool exit = false;
+bool correctEntry = false;
+do
+{
+    Console.Write("\nEnter Employee's name: ");
+    string name = Console.ReadLine().ToLower().Trim();
+    firstNames.Add(name);
+
+    // Exit Application Logic
+    string addEmployee = string.Empty;
+    Console.Write("\nAdd another employee? Yes or No: ");
+    addEmployee = Console.ReadLine().ToLower().Trim();
+
+    if (addEmployee == "yes" || addEmployee == "no")
+    {
+        correctEntry = true;
+    } else
+    {
+        correctEntry = false;
+    }
+    while (!correctEntry)
+    {
+        Console.WriteLine("Please enter a valid command.");
+        Console.Write("\tAdd another employee? Yes or No: ");
+        addEmployee = Console.ReadLine().ToLower().Trim();
+        if (addEmployee == "yes" || addEmployee == "no")
+        {
+            correctEntry = true;
+        }
+    }
+    if (addEmployee == "no")
+    {
+        exit = true;
+    }
+} while (!exit);
+
+Console.WriteLine("\nCurrent Employees:");
+foreach (string firstName in firstNames)
+{
+    Console.WriteLine($"{firstName}");
+}
