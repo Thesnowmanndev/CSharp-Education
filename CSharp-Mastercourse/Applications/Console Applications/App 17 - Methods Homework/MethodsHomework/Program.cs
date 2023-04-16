6d557e5ffb20c2f921e619848004a2959a8a156f
Kyle Martin <kyle.martin.0819@gmail.com>
2023-04-16 14:59:15 -0400

    Update Program.cs

--- CSharp-Mastercourse/Applications/Console Applications/App 17 - Methods Homework/MethodsHomework/Program.cs
+++ CSharp-Mastercourse/Applications/Console Applications/App 17 - Methods Homework/MethodsHomework/Program.cs
@@ -1,11 +1,5 @@
 ﻿using MethodsHomework;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Greetings.Welcome();
-        (string firstName, string lastName) = User.GetNames();
-        Greetings.GreetUser(firstName, lastName);
-    }
-}
+Greetings.Welcome();
+(string firstName, string lastName) = User.GetNames();
+Greetings.GreetUser(firstName, lastName);
