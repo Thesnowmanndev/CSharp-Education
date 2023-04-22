611de86b662ca38b6e43437d9bdf873ca5e1ef81
Kyle Martin <kyle.martin.0819@gmail.com>
2023-04-22 10:46:52 -0400

    Removed unnecessary usings

--- CSharp-Mastercourse/Applications/Console Applications/App 18 - Guest book/GuestBook/Greeting.cs
+++ CSharp-Mastercourse/Applications/Console Applications/App 18 - Guest book/GuestBook/Greeting.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GuestBook
+﻿namespace GuestBook
 {
     public class Greeting
     {
