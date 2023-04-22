9782f1e5cb1a7e87183d12d39ca5077782e4293e
Kyle Martin <kyle.martin.0819@gmail.com>
2023-04-22 19:11:24 -0400

    Finished Static Class Homework

---
+++ CSharp-Mastercourse/Applications/Console Applications/App 20 - Static Classes Homework/Readme.md
@@ -0,0 +1,9 @@
+# Static Classes - Homework
+
+## Application requirements 
+
+Create a console application with a static class that handles calculations.
+
+## Application Example
+
+To see the code source: Program.cs, CalculateData.cs
---
+++ CSharp-Mastercourse/Applications/Console Applications/App 20 - Static Classes Homework/StaticClassesHomework/Program.cs
@@ -0,0 +1,17 @@
+﻿using StaticClassesHomework;
+
+List<decimal> items = new();
+
+ControlData.InputItems(items);
+decimal accountBalance = ControlData.InputBalance();
+decimal totalCost = CalculateData.Add(items);
+decimal newAccountBalance = CalculateData.SubtractFromBalance(accountBalance, totalCost);
+
+GenerateMessages.PrintItems(items);
+
+string totalCostText = GenerateMessages.PrintTotal(items);
+Console.WriteLine(totalCostText);
+
+string accountBalanceText = GenerateMessages.PrintNewAccountBalance(accountBalance, totalCost);
+Console.WriteLine(accountBalanceText);
+
