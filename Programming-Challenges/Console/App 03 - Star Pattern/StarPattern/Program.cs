11ce12b5940e663f6638adc9e90a8da9fb676c3a
Kyle Martin <kyle.martin.0819@gmail.com>
2023-05-07 12:05:17 -0400

    Finished Star Pattern

---
+++ Programming-Challenges/Console/App 03 - Star Pattern/StarPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿int size = 4;
+
+// Top side of star
+for  (int i = 1; i <= size; i++)
+{
+    for (int j = 1; j <= size - i; j++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int k = 1; k <= 2 * i - 1; k++)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine();
+}
+
+// Bottom side of star
+for (int i = size - 1; i >= 1; i--)
+{
+    for (int j = 1; j <= size - i; j++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int k = 1; k <= 2 * i - 1; k++)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine();
+}
