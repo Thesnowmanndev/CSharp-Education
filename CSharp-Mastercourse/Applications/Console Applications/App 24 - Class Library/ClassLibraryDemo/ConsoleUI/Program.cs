bdb6038dba590bc70cfbeed32565e76ef54b545e
Kyle Martin <kyle.martin.0819@gmail.com>
2023-05-21 20:02:26 -0400

    Finished Class Library Example

---
+++ CSharp-Mastercourse/Applications/Console Applications/App 24 - Class Library/ClassLibraryDemo/ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using DemoLibrary.Models;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        ToadModel BlueToad = new ToadModel("Toad", "Blue", "White", 16);
+        ToadModel YellowToad = new ToadModel("Jim", "White", "Yellow", 18);
+        ToadModel PinkToad = new ToadModel("Amy", "Pink", "White", 21);
+
+        BlueToad.Greet();
+        YellowToad.Greet();
+        PinkToad.Greet();
+
+        Console.ReadLine();
+    }
+}
