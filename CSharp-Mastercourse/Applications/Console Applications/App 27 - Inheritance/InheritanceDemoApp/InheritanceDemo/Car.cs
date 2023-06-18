313212f87a8d3eb4f7232fc083ac8e974ae178f7
Kyle Martin <kyle.martin.0819@gmail.com>
2023-06-17 21:49:52 -0400

    Finished Inheritance Example

---
+++ CSharp-Mastercourse/Applications/Console Applications/App 27 - Inheritance/InheritanceDemoApp/InheritanceDemo/Car.cs
@@ -0,0 +1,23 @@
+﻿namespace InheritanceDemo
+{
+    class Car : Vehicle 
+    {
+        public string CarClassification { get; set; }   
+        public string BodyStyle { get; set; }
+        public string EngineType { get; set; }
+        public string TransmissionType { get; set; }
+        public string Drivetrain { get; set; }
+        public string FuelType { get; set; }
+
+        public string[] Features { get; set; }
+
+        public int FuelCapacity { get; set; }
+        public int FuelConsumption { get; set; }
+        public int NumberOfWheels { get; set; }
+
+        public float EngineSize { get; set; }
+        public float AccelerationTime { get; set; }
+
+        public bool HasAirbags { get; set; }
+    }
+}
