9066cd72f7f04b3c78a31805f94fb897d93616f1
Kyle Martin <kyle.martin.0819@gmail.com>
2023-06-17 21:50:16 -0400

    Finished Inheritance Demo

---
+++ CSharp-Mastercourse/Applications/Console Applications/App 27 - Inheritance/InheritanceDemoApp/InheritanceDemo/Boat.cs
@@ -0,0 +1,21 @@
+﻿namespace InheritanceDemo
+{
+    class Boat : Vehicle
+    {
+        public string BoatType { get; set; } // Sailboat, Motorboat, Yacht
+        public string WatercraftType { get; set; } // Fishing Boat, Speedboat, Cruiser
+        public string HullMaterial { get; set; }
+        public string PropulsionType { get; set; }
+
+        public string[] SafetyEquipment { get; set; }
+
+        public int Draft { get; set; }
+        public int MaximumCapacity { get; set; }
+        public int FuelCapacity { get; set; }
+
+        public float Length { get; set; }
+
+        public bool HasNavigationSystem { get; set; }
+        public bool HasCabin { get; set; }
+    }
+}
---
+++ CSharp-Mastercourse/Applications/Console Applications/App 27 - Inheritance/InheritanceDemoApp/InheritanceDemo/Motorcycle.cs
@@ -0,0 +1,13 @@
+﻿namespace InheritanceDemo
+{
+    class Motorcycle : Vehicle
+    {
+        public string MotorcycleType { get; set; } // Standard, Touring, Sport, ...
+        public string MotorcycleCategory { get; set; } // Lighweight, Middleweight, Heavy
+
+        public int EngineDisplacement { get; set; } // In ccs (400cc, 750cc, 1200cc)
+        public int FuelCapacity { get; set; }
+
+        public bool HasSideCar { get; set; }
+    }
+}
